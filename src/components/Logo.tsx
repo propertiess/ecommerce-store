609dba2eca8ef60fe180343d28f5a2d81e162e64
propertiess <erofeev2001@yandex.ru>
2023-03-07 10:24:12 +0300

    fix: prod error

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,14 +1,17 @@
 import { Flex, Title } from '@mantine/core';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import logo from '@/../public/logo.svg';
 
 import { A } from './A';
 
 export const Logo = () => {
+  const router = useRouter();
+
   return (
     <Flex gap='xs'>
-      <A href='/'>
+      <A active={router.asPath === '/'} href='/'>
         <Image width={43} height={43} src={logo} alt='Logo' />
       </A>
       <Title className='uppercase'>Shop</Title>
