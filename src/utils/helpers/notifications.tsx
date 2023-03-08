6add9107726f32698b1c3b4c9ec07e7ce9436b33
propertiess <erofeev2001@yandex.ru>
2023-03-08 23:00:03 +0300

    chore: add notification helpers

---
+++ src/utils/helpers/notifications.tsx
@@ -0,0 +1,22 @@
+import { showNotification } from '@mantine/notifications';
+import { IconCheck } from '@tabler/icons-react';
+
+export const showErrorNotification = (message: string): void => {
+  showNotification({
+    title: 'Ошибка',
+    message,
+    styles: () => ({
+      root: {
+        '&::before': { backgroundColor: 'red' }
+      }
+    })
+  });
+};
+
+export const showSuccessNotification = (message: string): void => {
+  showNotification({
+    title: 'Успех',
+    message,
+    icon: <IconCheck />
+  });
+};
