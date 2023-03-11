4529cb3b5f077aa7a76abd93d999139c461afd3f
propertiess <erofeev2001@yandex.ru>
2023-03-11 23:28:35 +0300

    feat: add guard for private page

--- pages/admin.tsx
+++ pages/admin.tsx
@@ -1,4 +1,15 @@
+import { GetServerSideProps } from 'next';
+
+import { withAuthAdmin } from '@/hooks/withAuth';
 import { AdminScreen } from '@/screens/admin/AdminScreen';
+
+export const getServerSideProps: GetServerSideProps = withAuthAdmin(
+  async () => {
+    return {
+      props: {}
+    };
+  }
+);
 
 const Admin = () => {
   return <AdminScreen />;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,7 @@
-// import { GetServerSideProps } from 'next';
-
-// import { withAuth } from '@/hooks/useAuth';
 import { HomeScreen } from '@/screens/home';
 
 const Home = () => {
   return <HomeScreen />;
 };
-
-// export const getServerSideProps: GetServerSideProps = withAuth(async () => {
-//   return {
-//     props: {}
-//   };
-// });
 
 export default Home;
