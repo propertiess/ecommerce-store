d0282468f3dd3f40fd35f918fdc708daa1555a19
propertiess <erofeev2001@yandex.ru>
2023-03-28 22:29:07 +0300

    refactor: remove ssg, add client loading

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,41 +1,21 @@
-import { Text } from '@mantine/core';
-import { GetStaticProps } from 'next';
+import { Text, Loader } from '@mantine/core';
 
 import { Layout } from '@/layout';
 import { HomeScreen } from '@/screens/home';
 import { ProductService } from '@/services/product/product.service';
 import { Product } from '@/types';
-
-export const getStaticProps: GetStaticProps<HomePageProps> = async () => {
-  try {
-    const products = await ProductService.getAll();
-
-    return {
-      props: {
-        products
-      },
-      revalidate: 60
-    };
-  } catch (e) {
-    return {
-      props: {},
-      revalidate: 60
-    };
-  }
-};
+import { useGetProducts } from '@/screens/home/hooks/useGetProducts';
 
 export type HomePageProps = {
   products?: Product[];
 };
 
-const Home = ({ products }: HomePageProps) => {
+const Home = () => {
+  const { data, isFetching } = useGetProducts();
+
   return (
     <Layout title='Магазин' description='Магазин товаров'>
-      {products ? (
-        <HomeScreen products={products} />
-      ) : (
-        <Text weight={500}>Произошла непредвиденная ошибка</Text>
-      )}
+      {!isFetching ? <>{data && <HomeScreen products={data} />}</> : <Loader />}
     </Layout>
   );
 };
