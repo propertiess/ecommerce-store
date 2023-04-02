9a3967118ae61536d0ef5c20e2e8378e81dcae1c
propertiess <erofeev2001@yandex.ru>
2023-04-02 13:29:16 +0300

    style: add icons with indicator

--- client/src/layout/header/UserAvatar.tsx
+++ client/src/layout/header/UserAvatar.tsx
@@ -1,12 +1,15 @@
-import { Avatar, List, Popover } from '@mantine/core';
+import { Avatar, Indicator, List, Popover, Tooltip } from '@mantine/core';
 import { observer } from 'mobx-react-lite';
+import { Heart, ShoppingBag } from 'tabler-icons-react';
 
 import { A } from '@/components/A';
 import { useAuthReset } from '@/hooks/useAuthReset';
-
-import { authorizationLinks } from './navbar';
+import { useBasketStore } from '@/store/basket/Basket';
+import { useLikedStore } from '@/store/liked/Liked';
 
 export const UserAvatar = observer(() => {
+  const { basket } = useBasketStore();
+  const { liked } = useLikedStore();
   const onResetAuth = useAuthReset();
 
   return (
@@ -15,12 +18,28 @@ export const UserAvatar = observer(() => {
         <Avatar radius='xl' size='md' />
       </Popover.Target>
       <Popover.Dropdown>
-        <List listStyleType='none' className='flex flex-col items-center'>
-          {authorizationLinks.map(link => (
-            <List.Item key={link.title}>
-              <A href={link.href}>{link.title}</A>
+        <List listStyleType='none' className='flex flex-col items-center gap-3'>
+          <List.Item>
+            <A href='/lk'>Профиль</A>
+          </List.Item>
+          <Tooltip label='Избранное'>
+            <List.Item>
+              <A href='/lk/liked'>
+                <Indicator label={liked.length} offset={3} size={16}>
+                  <Heart size={32} />
+                </Indicator>
+              </A>
             </List.Item>
-          ))}
+          </Tooltip>
+          <Tooltip label='Корзина'>
+            <List.Item>
+              <A href='/lk/basket'>
+                <Indicator label={basket.length} offset={3} size={16}>
+                  <ShoppingBag size={32} />
+                </Indicator>
+              </A>
+            </List.Item>
+          </Tooltip>
           <List.Item onClick={onResetAuth} className='cursor-pointer'>
             Выйти
           </List.Item>
