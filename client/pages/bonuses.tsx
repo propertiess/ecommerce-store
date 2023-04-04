1a165f07c5cf7fe68ce204770e448e584d3a209d
propertiess <erofeev2001@yandex.ru>
2023-04-04 11:31:46 +0300

    refactor: remove isr from bonuses page

--- client/pages/bonuses.tsx
+++ client/pages/bonuses.tsx
@@ -1,41 +1,14 @@
-import { GetStaticProps } from 'next';
-
 import { Layout } from '@/layout';
 import { BonusesScreen } from '@/screens/bonuses/BonusesScreen';
-import { Bonus } from '@/screens/bonuses/types';
-import { BonusService } from '@/services/bonus/bonus.service';
 import { NextPageWithAuth } from '@/types';
 
-export type BonusesPageProps = {
-  bonuses: Bonus[];
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  try {
-    const bonuses = await BonusService.getAll();
-
-    return {
-      props: {
-        bonuses
-      },
-      revalidate: 60
-    };
-  } catch (e) {
-    return {
-      notFound: true
-    };
-  }
-};
-
-const Bonuses: NextPageWithAuth<BonusesPageProps> = ({
-  bonuses
-}: BonusesPageProps) => {
+const Bonuses: NextPageWithAuth = () => {
   return (
     <Layout
       title='Скидочные купоны'
       description='Скидочные купоны для всей вашей семьи!'
     >
-      <BonusesScreen bonuses={bonuses} />
+      <BonusesScreen />
     </Layout>
   );
 };
--- client/src/screens/bonuses/BonusesScreen.tsx
+++ client/src/screens/bonuses/BonusesScreen.tsx
@@ -1,31 +1,44 @@
 import { Grid } from '@mantine/core';
+import { useQuery } from '@tanstack/react-query';
 import { observer } from 'mobx-react-lite';
-import { BonusesPageProps } from 'pages/bonuses';
 
+import { CenteredLoader } from '@/components/CenteredLoader';
+import { BonusService } from '@/services/bonus/bonus.service';
 import { useAuthStore } from '@/store/auth/Auth';
 import { useBasketStore } from '@/store/basket/Basket';
 
 import { BonusCard } from './components/BonusCard';
 
-export const BonusesScreen = observer(({ bonuses }: BonusesPageProps) => {
+export const BonusesScreen = observer(() => {
   const { userId } = useAuthStore();
   const { percentDiscount, setPercentDiscount } = useBasketStore();
+
+  const { data: bonuses, isFetching } = useQuery({
+    queryKey: ['bonuses'],
+    queryFn: () => BonusService.getAll(),
+    staleTime: Infinity
+  });
 
   const onAddedBonus = (percent: number) => {
     setPercentDiscount(percent);
   };
 
+  if (isFetching) {
+    return <CenteredLoader />;
+  }
+
   return (
     <Grid py='md'>
-      {bonuses.map(bonus => (
-        <Grid.Col key={bonus.id} span={12} xs={6} sm={4} xl={2}>
-          <BonusCard
-            onAddedBonus={onAddedBonus}
-            disabled={!!percentDiscount || !userId}
-            {...bonus}
-          />
-        </Grid.Col>
-      ))}
+      {bonuses?.length &&
+        bonuses.map(bonus => (
+          <Grid.Col key={bonus.id} span={12} xs={6} sm={4} xl={2}>
+            <BonusCard
+              onAddedBonus={onAddedBonus}
+              disabled={!!percentDiscount || !userId}
+              {...bonus}
+            />
+          </Grid.Col>
+        ))}
     </Grid>
   );
 });
